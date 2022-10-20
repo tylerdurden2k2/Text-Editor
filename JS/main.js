287448ec32d80b30726f459ff47639d937cf6a98
Quang Huy <phamquanghuy2809@gmail.com>
2022-10-20 19:48:50 +0700

    first commit

---
+++ JS/main.js
@@ -0,0 +1,50 @@
+const buttons = document.querySelectorAll('.btn');
+const content = document.querySelector('.content');
+
+const fontSizeUp = document.querySelector('.fontSizeUp');
+const fontSizeDown = document.querySelector('.fontSizeDown');
+
+const fontSizeSpan = document.querySelector('.fontSize span');
+
+for(let i=0; i<buttons.length ;i++) {
+    buttons[i].addEventListener('click', (e) =>{
+        console.log(e.target)
+        let command = e.target.getAttribute('data');
+
+        if(command === "createLink" || command === "insertImage") {
+            let url = prompt("Link: ","http://");
+            document.execCommand(command,false,url);
+        }else{
+            document.execCommand(command,false,null);
+        }
+    })
+}
+
+let font="";
+let fontSize=18;
+
+function updateFont() {
+    let selectFont = document.querySelector('#select_font');
+    font = selectFont.options[selectFont.selectedIndex].value;
+
+    content.style = 'font-family:' + font + ";";
+}
+updateFont();
+
+function updateFontSizeSpan() {
+    fontSizeSpan.innerHTML = "";
+    fontSizeSpan.innerHTML = fontSize + "px";
+}
+updateFontSizeSpan();
+
+fontSizeUp.addEventListener('click', () =>{
+    fontSize++;
+    content.style = "font-size:" + fontSize + "px;";
+    updateFontSizeSpan();
+})
+
+fontSizeDown.addEventListener('click', () =>{
+    fontSize--;
+    content.style = "font-size:" + fontSize + "px;";
+    updateFontSizeSpan();
+})
